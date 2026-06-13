6d48b2272f1bf8446f4d70426a76bb27a2c3115c
Federico <fedequinti98@gmail.com>
2026-06-13 12:24:57 +0200

    Separazione in classi

---
+++ 2/button_holder.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QMainWindow, QPushButton
+
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        super().setWindowTitle("Button Holder App")
+        button = QPushButton("Press Me!")
+
+        # Set up the button as our central widget
+        self.setCentralWidget(button)
--- 2/main.py
+++ 2/main.py
@@ -1,34 +1,10 @@
-# Importo il modulo sys per accedere agli argomenti passati da riga di comando
 import sys
 
-# Importo le classi Qt necessarie:
-# QApplication = gestisce l'intera applicazione GUI
-# QWidget = finestra/widget base di Qt
-from PySide6.QtWidgets import QApplication, QWidget
+from button_holder import ButtonHolder
+from PySide6.QtWidgets import QApplication
 
-# Creo l'oggetto applicazione.
-# Deve esistere una sola QApplication per processo.
-# sys.argv permette a Qt di leggere eventuali parametri passati da terminale.
 app = QApplication(sys.argv)
 
-
-# Creo una finestra vuota.
-# QWidget è il widget base da cui derivano quasi tutti i controlli Qt.
-window = QWidget()
-
-# Rende visibile la finestra.
-# Senza questa chiamata la finestra esiste ma rimane nascosta.
+window = ButtonHolder()
 window.show()
-
-
-# Avvia l'event loop di Qt.
-# Da questo momento il programma resta in attesa di eventi:
-# - click del mouse
-# - pressione tasti
-# - ridimensionamento finestra
-# - timer
-# - segnali dei widget
-#
-# Quando l'utente chiude l'applicazione,
-# app.exec() termina e il programma si chiude.
 app.exec()
